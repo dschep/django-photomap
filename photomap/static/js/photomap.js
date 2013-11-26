2bf6fbc8ee7a8c6bb5638a7684ce98773c3e6256
Daniel Schep <dschep@gmail.com>
2013-11-25 20:44:45 -0500

    use api to place markers on map!

--- photomap/static/js/photomap.js
+++ photomap/static/js/photomap.js
@@ -4,4 +4,10 @@
     L.tileLayer('http://{s}.tile.opencyclemap.org/cycle/{z}/{x}/{y}.png', {
                     attribution: '&copy; OpenCycleMap, Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'
                 }).addTo(map)
+    $.getJSON('/api/v1/photo/?format=json', function(data) {
+        $.each(data.objects, function(i, item) {
+            L.marker(item.location).addTo(map)
+                .bindPopup('<img width=300 src="' + item.image + '">');
+        });
+    });
 })()

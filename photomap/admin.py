fc80212708d64ce56bb752c4222b69c58ea8cdd6
Daniel Schep <dschep@gmail.com>
2014-04-24 16:06:19 -0400

    actual photo admin

--- photomap/admin.py
+++ photomap/admin.py
@@ -2,4 +2,12 @@ from django.contrib import admin
 
 from .models import Photo
 
-admin.site.register(Photo)
+
+class PhotoAdmin(admin.ModelAdmin):
+    list_display = ('image', 'preview', 'location')
+    readonly_fields = ('preview', 'location')
+    def preview(self, obj):
+        return '<img height="250" src="{0}">'.format(obj.image.url)
+    preview.allow_tags = True
+
+admin.site.register(Photo, PhotoAdmin)

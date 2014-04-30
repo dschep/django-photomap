cf6446e4902de41dd10ee624b6f25fc27bfe18fd
Daniel Schep <dschep@gmail.com>
2014-04-30 14:11:21 -0400

    populate location data from EXIF

--- photomap/api.py
+++ photomap/api.py
@@ -2,7 +2,7 @@ from tastypie.resources import ModelResource
 from tastypie.authorization import Authorization
 from tastypie import fields
 from .models import Photo
-from .util import MultipartResource, UserSessionKeyAuthorization
+from .util import MultipartResource, UserSessionKeyAuthorization, point_from_exif
 from django.contrib.gis.geos import geometry
 
 
@@ -17,8 +17,12 @@ class PhotoResource(MultipartResource, ModelResource):
             return [bundle.obj.location.y, bundle.obj.location.x]
 
     def hydrate_location(self, bundle):
-        """ create a Point object from an array """
-        if 'location' in bundle.data:
+        """
+        create a Point object from an array
+        unless it's already a point because it came from the image's EXIF
+        """
+        if 'location' in bundle.data and \
+                not isinstance(bundle.data['location'], geometry.Point):
             bundle.data['location'] = geometry.Point(
                 x=bundle.data['location'][1],
                 y=bundle.data['location'][0],
@@ -34,3 +38,8 @@ class PhotoResource(MultipartResource, ModelResource):
 
     def obj_create(self, bundle, **kwargs):
         return super(PhotoResource, self).obj_create(bundle, user_session_key=bundle.request.session.session_key)
+
+    def hydrate_image(self, bundle):
+        if 'image' in bundle.data:
+            bundle.data['location'] = point_from_exif(bundle.data['image'])
+        return bundle

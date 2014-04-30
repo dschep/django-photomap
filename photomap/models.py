43adb3f8532d8d035ec059e028b948fade72a336
Daniel Schep <dschep@gmail.com>
2014-04-30 01:09:41 -0400

    unicode representation of the Photo model

--- photomap/models.py
+++ photomap/models.py
@@ -5,6 +5,9 @@ class Photo(models.Model):
     location = models.PointField(srid=4326, null=True)
     objects = models.GeoManager()
 
+    def __unicode__(self):
+        return "Image('{0.image.name}', '{0.location}')".format(self)
+
 
 from south.modelsinspector import add_introspection_rules
 add_introspection_rules([], ["^django\.contrib\.gis"])

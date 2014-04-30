a4876351a7e455ca0ecdfb083da30dde53a9ba11
Daniel Schep <dschep@gmail.com>
2014-04-30 11:46:36 -0400

    hydrate location into a Point

--- photomap/api.py
+++ photomap/api.py
@@ -2,6 +2,7 @@ from tastypie.resources import ModelResource
 from tastypie import fields
 from .models import Photo
 from .util import MultipartResource
+from django.contrib.gis.geos import geometry
 
 
 class PhotoResource(MultipartResource, ModelResource):
@@ -12,3 +13,14 @@ class PhotoResource(MultipartResource, ModelResource):
         """ flip coordinate order since leaflet expects lat,lng not lng,lat """
         if bundle.obj.location is not None:
             return [bundle.obj.location.y, bundle.obj.location.x]
+
+    def hydrate_location(self, bundle):
+        """ create a Point object from an array """
+        if 'location' in bundle.data:
+            bundle.data['location'] = geometry.Point(
+                x=bundle.data['location'][1],
+                y=bundle.data['location'][0],
+                srid=4326,
+                )
+
+        return bundle

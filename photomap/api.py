30bcc6ca40e6093ab2679edcc6793fa301676723
Daniel Schep <dschep@gmail.com>
2014-05-23 22:52:57 -0400

    fix some line lengths

--- photomap/api.py
+++ photomap/api.py
@@ -18,12 +18,14 @@ class PhotoResource(MultipartResource, ModelResource):
         return bundle
 
     def obj_create(self, bundle, **kwargs):
-        return super(PhotoResource, self).obj_create(bundle, user_session_key=bundle.request.session.session_key)
+        return super(PhotoResource, self).obj_create(
+                bundle, user_session_key=bundle.request.session.session_key)
 
     def hydrate_image(self, bundle):
         if 'image' in bundle.data:
             try:
-                bundle.data['location'] = geojson_from_exif(bundle.data['image'])
+                bundle.data['location'] = geojson_from_exif(
+                        bundle.data['image'])
             except NoGPSInfoException:
                 pass
         return bundle
